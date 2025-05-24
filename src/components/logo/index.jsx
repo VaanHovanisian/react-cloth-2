d9ea30315b30224e1046042deacad5609b86edc6
VaanHovanisian <hovanisianvaan@gmail.com>
2025-05-24 08:15:54 +0400

    update

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import Styles from "./style.module.css"
+import logo from "../../assets/logo.png";
+import Styles from "./style.module.css";
 import { Link } from "react-router";
 
-
 export const Logo = () => {
-return (
-<Link to="/" >
-<div className={Styles.logo}>
-    <img className={Styles.img} src="public/Akatsuki-Logo-PNG-Pic.png" width="45" height="45" />
-    <b className={Styles.b}>REACT CLOTH</b>
-    
-    <strong className={Styles.strong}>the best clothes in the world</strong>
-    </div>
-</Link>
-);
-}
+  return (
+    <Link to="/">
+      <div className={Styles.logo}>
+        <img
+          className={Styles.img}
+          src="/Akatsuki-Logo-PNG-Pic.png"
+          width="45"
+          height="45"
+        />
+        <b className={Styles.b}>REACT CLOTH</b>
+
+        <strong className={Styles.strong}>the best clothes in the world</strong>
+      </div>
+    </Link>
+  );
+};
