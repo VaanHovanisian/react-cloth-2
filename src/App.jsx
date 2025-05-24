89b6b5105a2d1f782b5562a2454ec102c85fea5a
VaanHovanisian <hovanisianvaan@gmail.com>
2025-05-24 12:40:08 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import "./style/global.css"
-import "./style/repeat.css"
+import "./style/global.css";
+import "./style/repeat.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { Home, Cart, ProductPage, Order } from "./pages";
+import { Header } from "./components/header";
 
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router"
-import { Home, Cart, ProductPage, Order } from "./pages"
-import { Header } from "./components/header"
-
-import { Suspense } from "react"
-
-
-
+import { Suspense } from "react";
 
 function App() {
-
   return (
     <Router>
       <Header />
-      <Suspense >
+      <Suspense>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<Order />} />
-
+          <Route path="/product/:id" element={<ProductPage />} />
           <Route path="*" element={<h1>Not found Page 404</h1>} />
-          <Route path="/product/:id" element={<ProductPage />}></Route>
-
-
         </Routes>
       </Suspense>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/catalog/index.jsx
+++ src/components/catalog/index.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import styles from "./style.module.css";
 import { Card } from "../card";
-
 import { Skeleton } from "../card/skeleton";
 
 export const Catalog = ({ data, error, isLoading }) => {
