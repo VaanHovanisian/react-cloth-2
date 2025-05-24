dd793c55f3a17b9a1e76c85e9a96f8c7716073cf
VaanHovanisian <hovanisianvaan@gmail.com>
2025-05-24 11:53:13 +0400

    update

--- src/hooks/basket.js
+++ src/hooks/basket.js
@@ -1,63 +1,56 @@
-import useSWR from "swr"
-import { fetcher } from "../helpers/fetcher.js"
+import useSWR from "swr";
+import { fetcher } from "../helpers/fetcher.js";
 import { ApiRoutes } from "../services/constants.js";
 import { axiosInstance } from "../services/instance.js";
 
-
 export const useBasket = () => {
-    const { data: products, mutate, error, isLoading } = useSWR(ApiRoutes.BASKET, fetcher)
-    const data = products?.length > 0 ? products : [];
-    const isToBasket = (id) => data.some(item => item.productId === id)
-    const isFindToBasket = (id) => data.find(item => item.productId === id)
+  const {
+    data: products,
+    mutate,
+    error,
+    isLoading,
+  } = useSWR(ApiRoutes.BASKET, fetcher);
+  const data = products?.length > 0 ? products : [];
+  const isToBasket = (id) => data.some((item) => item.productId === id);
+  const isFindToBasket = (id) => data.find((item) => item.productId === id);
 
+  const addProduct = async (product) => {
+    if (isToBasket(product.id)) return;
+    const { id, title, price, type, img } = product;
+    const objProduct = { title, price, type, img, productId: id, count: 1 };
 
-    const addProduct = async (product) => {
+    await axiosInstance.post(ApiRoutes.BASKET, objProduct);
 
-        if (isToBasket(product.id)) return;
-        const { id, title, price, type, img } = product;
+    mutate([...data, objProduct]);
+  };
 
-        await axiosInstance.post(ApiRoutes.BASKET, {
-            title,
-            price,
-            type,
-            img,
-            productId: id,
-            count: 1
-        })
+  const removeProduct = async (id) => {
+    await axiosInstance.delete(`${ApiRoutes.BASKET}/${id}`);
+    mutate(data.filter((item) => item.productId !== id));
+  };
+  const updateProduct = async (product) => {
+    await axiosInstance.put(`${ApiRoutes.BASKET}/${product.id}`, product);
 
+    mutate(data.filter((item) => (item.id === product.id ? product : item)));
+  };
 
+  const clearProducts = async () => {
+    data.forEach(async ({ id }) => await removeProduct(id));
+  };
+  const totalPrice = data.reduce((acc, el) => acc + el.price * el.count, 0);
+  const totalCount = data.reduce((acc, el) => acc + el.count, 0);
 
-        mutate([...data, product])
-    }
-
-    const removeProduct = async (id) => {
-        await axiosInstance.delete(`${ApiRoutes.BASKET}/${id}`)
-        mutate(data.filter(item => item.productId !== id))
-    }
-    const updateProduct = async (product) => {
-        await axiosInstance.put(`${ApiRoutes.BASKET}/${product.id}`, product)
-
-        mutate(data.filter(item => item.id === product.id ? product : item))
-
-    }
-
-    const clearProducts = async () => {
-        data.forEach(async ({ id }) => await removeProduct(id));
-    }
-    const totalPrice = data.reduce((acc, el) => acc + el.price * el.count, 0);
-    const totalCount = data.reduce((acc, el) => acc + el.count, 0);
-
-    return {
-        totalPrice,
-        totalCount,
-        isFindToBasket,
-        clearProducts,
-        removeProduct,
-        updateProduct,
-        addProduct,
-        isToBasket,
-        data,
-        error,
-        isLoading
-    }
-}
+  return {
+    totalPrice,
+    totalCount,
+    isFindToBasket,
+    clearProducts,
+    removeProduct,
+    updateProduct,
+    addProduct,
+    isToBasket,
+    data,
+    error,
+    isLoading,
+  };
+};
